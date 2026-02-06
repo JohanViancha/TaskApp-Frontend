d00874ab99b5412138b2923d8bac13e069fd00ed
Johan Viancha <vianchajohan@gmail.com>
2026-02-06 18:25:21 -0500

    agregar environment para produccion

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  apiUrl: 'https://us-central1-taskapp-1ffe0.cloudfunctions.net/api',
+};
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
-  production: true,
-  apiUrl: 'https://us-central1-taskapp-1ffe0.cloudfunctions.net/api',
+  production: false,
+  apiUrl: 'http://127.0.0.1:5001/taskapp-1ffe0/us-central1/api',
 };
