888642157ffe1cda510ce340e198205d6f0bc1e2
Johan Viancha <vianchajohan@gmail.com>
2026-02-06 23:16:37 -0500

    ajustar interceptor

--- src/app/core/interceptors/auth.interceptor.ts
+++ src/app/core/interceptors/auth.interceptor.ts
@@ -1,9 +1,13 @@
 import type { HttpInterceptorFn } from '@angular/common/http';
+import { WITH_CREDENTIALS } from '../../shared/models/user.context.model';
 
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
-  const clone = req.clone({
-    withCredentials: true,
-  });
+ const useCredentials = req.context.get(WITH_CREDENTIALS);
 
-  return next(clone);
+  if (useCredentials) {
+    const clonedReq = req.clone({ withCredentials: true });
+    return next(clonedReq);
+  }
+
+  return next(req);
 };
